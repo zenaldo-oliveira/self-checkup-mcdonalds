84b696f4b3a34058e4503e6a4248e61f3e985323
Zenaldo Oliveira <zenaldo.221@hotmail.com>
2025-05-16 15:56:10 -0400

    feat: adiciona componentes Button e Input com Shadcn UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const HomePage = () => {
   return (
-    <div className="p-6 border-red-300 rounded-lg border-2 ">
+    <div className="p-5 border-red-500 rounded-xl ">
       <h1 className="text-red-500">hello world</h1>
+      <Button>FWS 7.0</Button>
+      <Input placeholder="Bora fechar com felipao"></Input>
     </div>
   );
 };
